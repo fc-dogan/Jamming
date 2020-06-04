0f3caacb13cdac3af766194b000da1970fe3d4c9
Fatma <fceyda@gmail.com>
2020-06-04 13:57:22 -0700

    add updatePlaylistName method to App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -49,6 +49,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track){
@@ -66,6 +67,10 @@ class App extends React.Component {
     let tracks = this.state.playlistTracks;
     tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
     this.setState( { playlistTracks: tracks});
+  }
+
+  updatePlaylistName(name){
+    this.setState( {playlistName: name});
   }
 
   render() {
