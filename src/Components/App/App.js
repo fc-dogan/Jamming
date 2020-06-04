7bdc6ad03a180256c6521f0ace113464d434b8c6
Fatma <fceyda@gmail.com>
2020-06-03 22:06:22 -0700

    create removeTrack method in the Track.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -48,6 +48,7 @@ class App extends React.Component {
     };
 
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track){
@@ -75,7 +76,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -7,6 +7,7 @@ class Track extends React.Component {
     super(props);
     // this.renderAction =this.renderAction.bind(this);
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   
@@ -20,6 +21,10 @@ class Track extends React.Component {
   
   addTrack(){
     this.props.onAdd(this.props.track);
+  }
+
+  removeTrack(){
+    this.props.onRemove(this.props.tracks);
   }
 
 
