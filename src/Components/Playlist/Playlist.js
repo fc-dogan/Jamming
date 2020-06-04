f130f853bd2a3411e136c0800ac0b9ba00207322
Fatma <fceyda@gmail.com>
2020-06-04 14:32:44 -0700

    create a savePlaylist method that saves the playlist to users account

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -18,8 +18,10 @@ class Playlist extends React.Component {
     return (
       <div className="Playlist">
         <input defaultValue={'New Playlist'} onChange={this.handleNameChange} />
-        <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <TrackList tracks={this.props.playlistTracks}
+                   onRemove={this.props.onRemove} 
+                   isRemoval={true} />
+        <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
       </div>
     );
   }
