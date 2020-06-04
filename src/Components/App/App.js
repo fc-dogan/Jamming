a767b23c5d4343dc13b420481b9e3b57ff972821
Fatma <fceyda@gmail.com>
2020-06-03 21:58:48 -0700

    add removeTrack method to App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -61,6 +61,12 @@ class App extends React.Component {
     
   }
 
+  removeTrack(track){
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+    this.setState( { playlistTracks: tracks});
+  }
+
   render() {
     return (
       <div>
